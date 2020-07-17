28bbd620c295c5222a4dd9dc16948a17af1c5ace
Hielke <hielkehielkema93@gmail.com>
2020-07-17 19:56:14 +0200

    Add automated cat facts

---
+++ TEMPLATE.md
@@ -0,0 +1,26 @@
+### Hello there 👋
+
+I work for [Ydentic B.V.](https://github.com/Ydentic) as the lead software developer/architect. I like to work on new technologies and ideas in my spare time. I specialize in the .NET stack, web development, security, identity/access management and code scalability.
+
+- 📫 How to reach me: hielke@ydentic.com
+- 💬 Languages: Dutch and English
+
+**🐈 Fact of the day:**
+```
+[PLACEHOLDER]
+```
+
+<!--
+**hlhielkema/hlhielkema** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
---
+++ build-md.js
@@ -0,0 +1,53 @@
+const https = require('https');
+const fs = require('fs');
+
+// Configuration
+var API_URI = 'https://cat-fact.herokuapp.com/facts/random';
+var TEMPLATE_FILENAME = 'TEMPLATE.md';
+var TEMPLATE_PLACEHOLDER = '[PLACEHOLDER]';
+var TARGET_FILENAME = 'README.md'
+
+function getCatFact(callback)
+{
+    console.log('Fetch cat fact with API');
+    https.get(API_URI, (resp) => {
+        let data = '';        
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });      
+        resp.on('end', () => {
+            callback(JSON.parse(data).text);
+        });      
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
+
+function writeMarkdown(text) {
+    console.log('Write ' + TARGET_FILENAME);
+    fs.writeFile(TARGET_FILENAME, text, function (err) {
+        if (err) {
+            return console.log(err);
+        }        
+    });
+}
+
+function readTemplate(callback) {
+    console.log('Read ' + TEMPLATE_FILENAME);
+    fs.readFile(TEMPLATE_FILENAME, 'utf8', function(err, contents) {
+        callback(contents);
+    });
+}
+
+function populateTemplate(template, text) {
+    console.log('Generate the README contents')
+    return template.replace(TEMPLATE_PLACEHOLDER, text);
+}
+
+getCatFact(function(text) {
+    console.log('Fact: ', text);
+    readTemplate(function(template) {                
+        template = populateTemplate(template, text);
+        writeMarkdown(template);
+    });    
+});
